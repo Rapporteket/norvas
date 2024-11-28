ce9fbe3bde6d9b941bab645accf1badaec1ffad8
kevinthon <kevin.thon@gmail.com>
2024-11-28 16:12:13 +0100

    utvikling

--- doc/bestilling_november.R
+++ doc/bestilling_november.R
@@ -10,17 +10,19 @@ rap_aar <- 2024
 aarrappdata <- norvas::lesogprosesser(rap_aar = rap_aar)
 Inklusjon <- aarrappdata$Inklusjon
 Oppfolging <- aarrappdata$Oppfolging
+VaskulittIntervensjon <- aarrappdata$VaskulittIntervensjon
 # Diagnoser <- aarrappdata$Diagnoser
 # Medisiner <- aarrappdata$Medisiner
-BVAS <- aarrappdata$BVAS
-KERR <- aarrappdata$KERR
-VDI <- aarrappdata$VDI
-VaskulittIntervensjon <- aarrappdata$VaskulittIntervensjon
+# BVAS <- aarrappdata$BVAS
+# KERR <- aarrappdata$KERR
+# VDI <- aarrappdata$VDI
 # Alvorlig_infeksjon <- aarrappdata$Alvorlig_infeksjon
 # Utredning <- aarrappdata$Utredning
 # Labskjema <- aarrappdata$Labskjema
 # Pasientsvar <- aarrappdata$Pasientsvar
 
+
+## Inklusjon og oppfølging samlet
 Inkl_oppf <- dplyr::bind_rows(
   Inklusjon[,c("PasientGUID", "InklusjonDato", "Inklusjonsaar",
                "FormStatus", "Diag_gr_nr", "Diagnose_ny_30", "Sykehusnavn")] %>%
@@ -37,7 +39,7 @@ Inkl_oppf <- dplyr::bind_rows(
   )) %>%
   dplyr::filter(FormStatus == 2)
 
-# Andel inklusjon/oppfølging med tilhørende VDI-skjema
+# Andel inklusjon/oppfølging med tilhørende VaskulittIntervensjonsskjema
 vasint <- merge(Inkl_oppf,
                 VaskulittIntervensjon[, c("TrimetoprimSulfa", "PasientGUID",
                                           "VaskulittIntervensjon_Dato")],
@@ -52,22 +54,25 @@ vasint <- merge(Inkl_oppf,
   arrange(Aar)
 
 
+
 Sdomdebut <- Inkl_oppf %>%
   dplyr::filter(Diagnose_ny_30 == 1,
                 Aar >= rap_aar-2) %>%
   dplyr::inner_join(
     VaskulittIntervensjon %>%
       dplyr::select(PasientGUID, VaskulittIntervensjon_Dato,
-                    CaVitaminD, BisfosfonatEllerTilsvarende),
+                    CaVitaminD, BisfosfonatEllerTilsvarende,
+                    TrimetoprimSulfa, Annenantibiotikaprofylakse),
     by = join_by(PasientGUID, Dato == VaskulittIntervensjon_Dato)
   ) %>%
   dplyr::summarise(
     CaVitaminD = sum(CaVitaminD),
     BisfosfonatEllerTilsvarende = sum(BisfosfonatEllerTilsvarende),
+    TrimetoprimSulfa = sum(TrimetoprimSulfa),
+    Annenantibiotikaprofylakse = sum(Annenantibiotikaprofylakse),
     N = dplyr::n(),
     .by = c(Sykehusnavn, Aar, Diag_gr)
   )
-
 
 oppf_6mnd <- dplyr::inner_join(
   Inkl_oppf %>%
@@ -89,41 +94,158 @@ oppf_6mnd <- dplyr::inner_join(
   dplyr::inner_join(
     VaskulittIntervensjon %>%
       dplyr::select(PasientGUID, VaskulittIntervensjon_Dato,
-                    CaVitaminD, BisfosfonatEllerTilsvarende),
+                    CaVitaminD, BisfosfonatEllerTilsvarende,
+                    TrimetoprimSulfa, Annenantibiotikaprofylakse),
     by = join_by(PasientGUID, Dato.y == VaskulittIntervensjon_Dato)
   ) %>%
   dplyr::summarise(
     CaVitaminD = sum(CaVitaminD),
     BisfosfonatEllerTilsvarende = sum(BisfosfonatEllerTilsvarende),
+    TrimetoprimSulfa = sum(TrimetoprimSulfa),
+    Annenantibiotikaprofylakse = sum(Annenantibiotikaprofylakse),
     N = dplyr::n(),
     .by = c(Sykehusnavn, Aar, Diag_gr)
+  ) %>%
+  dplyr::filter(Aar >= rap_aar-2)
+
+oppf_12mnd <- dplyr::inner_join(
+  Inkl_oppf %>%
+    dplyr::select(-Aar) %>%
+    dplyr::filter(Diagnose_ny_30==1),
+  Inkl_oppf %>%
+    dplyr::filter(is.na(Diagnose_ny_30)) %>%
+    dplyr::select(PasientGUID, Dato, Aar),
+  by = join_by(PasientGUID)
+) %>%
+  dplyr::mutate(
+    tid_fra_debut = difftime(Dato.y, Dato.x, units = "days"),
+    tid_fra_6mnd = abs(tid_fra_debut-365)
+  ) %>%
+  dplyr::filter(tid_fra_6mnd == min(tid_fra_6mnd), .by = PasientGUID) %>%
+  dplyr::distinct(Dato.y, PasientGUID, .keep_all = TRUE) %>%
+  dplyr::filter(tid_fra_6mnd <= 90) %>%
+  dplyr::distinct(tid_fra_6mnd, PasientGUID, .keep_all = TRUE) %>%
+  dplyr::inner_join(
+    VaskulittIntervensjon %>%
+      dplyr::select(PasientGUID, VaskulittIntervensjon_Dato,
+                    CaVitaminD, BisfosfonatEllerTilsvarende,
+                    TrimetoprimSulfa, Annenantibiotikaprofylakse),
+    by = join_by(PasientGUID, Dato.y == VaskulittIntervensjon_Dato)
+  ) %>%
+  dplyr::summarise(
+    CaVitaminD = sum(CaVitaminD),
+    BisfosfonatEllerTilsvarende = sum(BisfosfonatEllerTilsvarende),
+    TrimetoprimSulfa = sum(TrimetoprimSulfa),
+    Annenantibiotikaprofylakse = sum(Annenantibiotikaprofylakse),
+    N = dplyr::n(),
+    .by = c(Sykehusnavn, Aar, Diag_gr)
+  ) %>%
+  dplyr::filter(Aar >= rap_aar-2)
+
+
+
+andel_pr_shus_aar_diaggr <- function(dataramme, diaggr, aar, variabel) {
+  dataramme %>%
+    dplyr::filter(
+      Diag_gr == diaggr,
+      Aar == aar) %>%
+    dplyr::select(Sykehusnavn, !! sym(variabel), N) %>%
+    janitor::adorn_totals() %>%
+    dplyr::mutate(Andel = paste0(round(!! sym(variabel)/N*100,1), "% (",
+                                 !! sym(variabel), " av ", N, ")")) %>%
+    dplyr::select(Sykehusnavn, Andel)
+}
+
+samlet_1aar_og_diaggr <- function(data1, data2, data3, aar, diaggr, variabel) {
+  dplyr::full_join(
+    andel_pr_shus_aar_diaggr(data1,
+                             diaggr = diaggr,
+                             aar = aar,
+                             variabel = variabel),
+    andel_pr_shus_aar_diaggr(data2,
+                             diaggr = diaggr,
+                             aar = aar,
+                             variabel = variabel),
+    by = "Sykehusnavn",
+    suffix = c("_debut", "_6mnd")
+  ) %>%
+    dplyr::full_join(
+      andel_pr_shus_aar_diaggr(data3,
+                               diaggr = diaggr,
+                               aar = aar,
+                               variabel = variabel),
+      by = "Sykehusnavn"
+    ) %>%
+    dplyr::rename(Andel_12mnd = Andel)
+}
+
+samle3aar <- function(Sdomdebut, oppf_6mnd, oppf_12mnd,
+                      diaggr = "Storkarsvaskulitt (LVV)",
+                      sisteaar = 2024,
+                      variabel = "CaVitaminD") {
+  dplyr::bind_rows(
+    samlet_1aar_og_diaggr(Sdomdebut, oppf_6mnd, oppf_12mnd,
+                          diaggr = diaggr,
+                          aar = sisteaar,
+                          variabel = variabel) %>%
+      dplyr::mutate(Aar = sisteaar) %>%
+      dplyr::relocate(Aar),
+    samlet_1aar_og_diaggr(Sdomdebut, oppf_6mnd, oppf_12mnd,
+                          diaggr = diaggr,
+                          aar = sisteaar-1,
+                          variabel = variabel) %>%
+      dplyr::mutate(Aar = sisteaar-1) %>%
+      dplyr::relocate(Aar),
+    samlet_1aar_og_diaggr(Sdomdebut, oppf_6mnd, oppf_12mnd,
+                          diaggr = diaggr,
+                          aar = sisteaar-2,
+                          variabel = variabel) %>%
+      dplyr::mutate(Aar = sisteaar-2) %>%
+      dplyr::relocate(Aar)
   )
+}
+
+Samlet_CaVitaminD_GCA <-
+  samle3aar(Sdomdebut, oppf_6mnd, oppf_12mnd,
+            diaggr = "Storkarsvaskulitt (LVV)",
+            sisteaar = 2024,
+            variabel = "CaVitaminD")
+Samlet_Bisfosfonat_GCA <-
+  samle3aar(Sdomdebut, oppf_6mnd, oppf_12mnd,
+            diaggr = "Storkarsvaskulitt (LVV)",
+            sisteaar = 2024,
+            variabel = "BisfosfonatEllerTilsvarende")
+Samlet_CaVitaminD_ANCA <-
+  samle3aar(Sdomdebut, oppf_6mnd, oppf_12mnd,
+            diaggr = "ANCA assosiert vaskulitt (AAV)",
+            sisteaar = 2024,
+            variabel = "CaVitaminD")
+Samlet_Bisfosfonat_ANCA <-
+  samle3aar(Sdomdebut, oppf_6mnd, oppf_12mnd,
+            diaggr = "ANCA assosiert vaskulitt (AAV)",
+            sisteaar = 2024,
+            variabel = "BisfosfonatEllerTilsvarende")
+Samlet_TrimetoprimSulfa_ANCA <-
+  samle3aar(Sdomdebut, oppf_6mnd, oppf_12mnd,
+            diaggr = "ANCA assosiert vaskulitt (AAV)",
+            sisteaar = 2024,
+            variabel = "TrimetoprimSulfa")
+Samlet_Annenantibiotikaprofylakse_ANCA <-
+  samle3aar(Sdomdebut, oppf_6mnd, oppf_12mnd,
+            diaggr = "ANCA assosiert vaskulitt (AAV)",
+            sisteaar = 2024,
+            variabel = "Annenantibiotikaprofylakse")
 
 
-
-CaVitaminD_GCA_2024 <- Sdomdebut %>%
-  dplyr::filter(
-    Diag_gr == "Storkarsvaskulitt (LVV)",
-    Aar == 2024) %>%
-  dplyr::select(-Aar, -Diag_gr) %>%
-  janitor::adorn_totals() %>%
-  dplyr::mutate(Andel = paste0(round(CaVitaminD/N*100,1), "% (", CaVitaminD, " av ", N, ")"))
-CaVitaminD_GCA_2023 <- Sdomdebut %>%
-  dplyr::filter(
-    Diag_gr == "Storkarsvaskulitt (LVV)",
-    Aar == 2023) %>%
-  dplyr::select(-Aar, -Diag_gr) %>%
-  janitor::adorn_totals() %>%
-  dplyr::mutate(Andel = paste0(round(CaVitaminD/N*100,1), "% (", CaVitaminD, " av ", N, ")"))
-CaVitaminD_GCA_2022 <- Sdomdebut %>%
-  dplyr::filter(
-    Diag_gr == "Storkarsvaskulitt (LVV)",
-    Aar == 2022) %>%
-  dplyr::select(-Aar, -Diag_gr) %>%
-  janitor::adorn_totals() %>%
-  dplyr::mutate(Andel = paste0(round(CaVitaminD/N*100,1), "% (", CaVitaminD, " av ", N, ")"))
-
-
+Diag_aar <- Inklusjon %>%
+  dplyr::filter(FormStatus == 2) %>%
+  dplyr::mutate(DiagnoseAar = format(Diagnose_Klinisk_Dato, format = "%Y") %>%
+                  as.numeric()) %>%
+  dplyr::count(DiagnoseAar, Diag_gr) %>%
+  tidyr::pivot_wider(names_from = Diag_gr, values_from = n) %>%
+  dplyr::arrange(-DiagnoseAar) %>%
+  dplyr::filter(DiagnoseAar >= rap_aar-2) %>%
+  janitor::adorn_totals()
 
 
 
